2889b652b7be3da162f979eca009246af1061988
sean huang <sean.huang@tarokosoftware.com>
2019-05-02 13:15:09 +0800

    155.156 Dynamically create ingredients component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import BurgerIngredient, { INGREDIENT_LIST } from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
+import { _ } from 'core-js';
 
-const Burger = porps => {
+const Burger = props => {
+
+  let ingredients = Object.keys(props.ingredients).map(ingredientName => {
+    const quantity = props.ingredients[ingredientName];
+
+    /* 
+      Array(2) will create empty array and length is 2 but empty array can not map 
+      use Array(2).fill() or Array.from(Array(2)) to push values to array
+    */
+    return Array(quantity).fill().map((_, index) => {
+      return <BurgerIngredient key={ingredientName + index} type={ingredientName} />
+    })
+  }).flat();
+  // console.log(ingredients) // [[{...}, {...}], [{...}], [{...}, {...}], [{...}]]
+
+  /* flatten array [{...}, {...}, {...}, {...}] */
+  ingredients.reduce((arr, value) => arr.concat(value), [])
+  // or use new way ingredients.flat() 
+
+  if (ingredients.length === 0) {
+    ingredients = <p>Please start adding ingredients!</p>;
+  }
+  
   return (
     <div className={classes.burger}>
       <BurgerIngredient type={INGREDIENT_LIST.BREAD_TOP} />
-      <BurgerIngredient type={INGREDIENT_LIST.CHEESE} />
-      <BurgerIngredient type={INGREDIENT_LIST.MEAT} />
+      {ingredients}
       <BurgerIngredient type={INGREDIENT_LIST.BREAD_BOTTOM} />
     </div>
   )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      bacon: 1,
+      cheese: 2,
+      salad: 1,
+      meat: 2
+    }
+  }
   render () {
     return (
       <Aux>
-        <Burger/>
+        <Burger ingredients={this.state.ingredients}/>
         <div>builder controls</div>
       </Aux>
     )
