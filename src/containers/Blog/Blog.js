32e1a1b44e3099b9bd71b2a197225cf2c669eedb
sean huang <sean.huang@tarokosoftware.com>
2019-05-07 11:10:19 +0800

    189. Creating a Http Request to GET Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Post from '../../components/Ajax/Post/Post';
-import FullPost from '../../components/Ajax/FullPost/FullPost';
-import NewPost from '../../components/Ajax/NewPost/NewPost';
-import classes from './Blog.css';
+import Post from "../../components/Ajax/Post/Post";
+import FullPost from "../../components/Ajax/FullPost/FullPost";
+import NewPost from "../../components/Ajax/NewPost/NewPost";
+import classes from "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className={classes.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log(response)
+      })
+  }
+  render() {
+    return (
+      <div>
+        <section className={classes.Posts}>
+          <Post />
+          <Post />
+          <Post />
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
