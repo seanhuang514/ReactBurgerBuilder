37a3c46fb8e5c4925dffebadb39e52d07442e7c1
sean huang <sean.huang@tarokosoftware.com>
2019-05-07 16:17:52 +0800

    198. Adding Interceptors to Execute Code Globally
    在全域攔截 axios 的 response & request 
    https://github.com/axios/axios#interceptors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import AjaxApp from './AjaxApp';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
 
+axios.interceptors.request.use(request => {
+  console.log('request', request);
+  return request;
+}, error => {
+  console.log('request-error', error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log('response', response);
+}, error => {
+  console.log('response-error', error)
+});
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<AjaxApp />, document.getElementById('root'));
 
