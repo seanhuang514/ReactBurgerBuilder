fae27a50cf195feed4e4c952d92235c24b5142b4
sean huang <sean.huang@tarokosoftware.com>
2019-05-08 14:45:26 +0800

    [Section 11] 216. Setting Up Links

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import AjaxApp from './AjaxApp';
 import * as serviceWorker from './serviceWorker';
-import axios from 'axios'
 
 /* 預設URL */
 // axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
@@ -13,21 +12,8 @@ import axios from 'axios'
 /* 預設content-type */
 // axios.defaults.headers.post['Content-type'] = 'application/json';
 
-axios.interceptors.request.use(request => {
-  console.log('request', request);
-  return request;
-}, error => {
-  console.log('request-error', error);
-});
-
-axios.interceptors.response.use(response => {
-  console.log('response', response);
-  return response
-}, error => {
-  console.log('response-error', error)
-});
-ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<AjaxApp />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AjaxApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
