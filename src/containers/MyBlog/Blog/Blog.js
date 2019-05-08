405fded4fa2167d9875fdcf392a23cf2eeb4dbc8
sean huang <sean.huang@tarokosoftware.com>
2019-05-08 15:30:11 +0800

    [note][Important][exact]220. Setting Up and Rendering Routes

--- src/containers/MyBlog/Blog/Blog.js
+++ src/containers/MyBlog/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./Blog.css";
 import Posts from '../Posts/Posts'
+import { Route } from 'react-router-dom';
 
 class Blog extends Component {
   render() {
@@ -14,7 +15,23 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts/>
+        
+        {/* 
+          Route 以 `/` 來匹配URL ，由URL 用 `\` 切開由長到短往下匹配
+          有 match 的就會render，以下面的範例來說如果 URL 是 /post 的話
+          只會 render `Home1`，如果 URL 是 /post/qqq 就會 render 
+          Home1 & Home2
+
+          <Route path="/post" render={() => <h1>Home1</h1>}/>
+          <Route path="/post/qqq" render={() => <h1>Home2</h1>}/>
+
+          但如果給 route 加上 exact 就會是完全匹配才會 render 所以到了
+          /post/qqq 也不會 render Home1
+          <Route path="/post" exact render={() => <h1>Home1</h1>}/>
+        */}
+
+        <Route path="/post" exact render={() => <h1>Home1</h1>}/>
+        <Route path="/post/qqq" render={() => <h1>Home2</h1>}/>
       </div>
     );
   }
