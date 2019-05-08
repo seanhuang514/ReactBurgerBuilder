7ec016427b954ecf4f9ba65803a627b1ce40e845
sean huang <sean.huang@tarokosoftware.com>
2019-05-08 23:17:28 +0800

    223. Using Links to Switch Pages(Link)
    用 Link 取代 a tag 來避免 reload page

--- src/containers/MyBlog/Blog/Blog.js
+++ src/containers/MyBlog/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import classes from "./Blog.css";
 import Posts from '../Posts/Posts'
 import NewPost from '../NewPost/NewPost';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class Blog extends Component {
   render() {
@@ -11,8 +11,10 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
+              {/* <li><a href="/">Home</a></li>
+              <li><a href="/new-post">New Post</a></li> */}
+              <li><Link to="/">Home</Link></li>
+              <li><Link to={{ pathname: '/new-post', hash: 'submit', search: 'q=1' }}>New Post</Link></li>
             </ul>
           </nav>
         </header>
