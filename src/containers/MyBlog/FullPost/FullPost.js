7df39bd68e9d11c9e364b2e845ce1e1af0d3a03c
sean huang <sean.huang@tarokosoftware.com>
2019-05-09 12:13:52 +0800

    230. Extracting Route Parameters
    use `this.props.match.params` to get URL params

--- src/containers/MyBlog/FullPost/FullPost.js
+++ src/containers/MyBlog/FullPost/FullPost.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../../axios';
 import classes from './FullPost.css';
 
 class FullPost extends Component {
   state = {
-    loadedPost: null
+    loadedPost: null,
+    id: null
   };
 
-  componentDidUpdate() {
-    if(!this.props.id || (this.state.loadedPost && this.state.loadedPost.id === this.props.id)) return
+  componentDidMount() {
+    const planId = this.props.match.params.id;
+    this.setState({ id: planId }, () => {
+      if(!planId || (this.state.loadedPost && this.state.loadedPost.id === planId)) return
 
-    axios.get('/posts/' + this.props.id)
-      .then(response => {
-        this.setState({ loadedPost: response.data });
-      })
-
-    // console.log('componentDidUpdate')
+      axios.get('/posts/' + planId)
+        .then(response => {
+          this.setState({ loadedPost: response.data });
+        })
+    });
   }
 
   deletePostHandler = () => {
-    axios.delete('/posts/' + this.props.id)
+    axios.delete('/posts/' + this.state.id)
       .then(response => {
-        this.setState({ loadedPost: null });
+        this.setState({ loadedPost: null, id: null });
         console.log('deletePostHandler', response);
       })
   }
 
   render() {
-    console.log('render-this.props.id', this.props.id)
-    if(!this.props.id) return <p style={{ textAlign: 'center' }}>Please select a Post!</p>;;
-    if(this.props.id && !this.state.loadedPost) return <p style={{ textAlign: 'center' }}>Loading data ....</p>;
+    const planId = this.state.id
+    if(!planId) return <p style={{ textAlign: 'center' }}>Please select a Post!</p>;;
+    if(planId && !this.state.loadedPost) return <p style={{ textAlign: 'center' }}>Loading data ....</p>;
     
     
     const post = (
