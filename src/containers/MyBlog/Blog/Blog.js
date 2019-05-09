885621add2f6f760ee3f5e221850cca582fec960
sean huang <sean.huang@tarokosoftware.com>
2019-05-09 12:48:24 +0800

    232. Using Switch to Load a Single Route
    use Switch to make sure we only load a single  route which is first match with path. Switch can prevent to we load multiple routes in single page

--- src/containers/MyBlog/Blog/Blog.js
+++ src/containers/MyBlog/Blog/Blog.js
@@ -3,7 +3,7 @@ import classes from "./Blog.css";
 import Posts from '../Posts/Posts'
 import NewPost from '../NewPost/NewPost';
 import FullPost from '../FullPost/FullPost';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 class Blog extends Component {
   render() {
@@ -45,10 +45,11 @@ class Blog extends Component {
         <Route path="/post/qqq" render={() => <h1>Home2</h1>}/> */}
 
         <Route path="/" exact component={Posts}/>
-        <Route path="/new-post" component={NewPost}/>
-        <Route path="/:id" exact component={FullPost}/>
-        
-        
+
+        <Switch>
+          <Route path="/new-post" component={NewPost}/>
+          <Route path="/:id" exact component={FullPost}/>
+        </Switch>
       </div>
     );
   }
