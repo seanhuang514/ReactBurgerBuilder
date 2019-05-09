0642e9b0f9966124e96cb4b54b734109694d5866
sean huang <sean.huang@tarokosoftware.com>
2019-05-09 10:15:59 +0800

    225. The "withRouter" HOC & Route Props
    The route props only pass to the component what is route to render. The child of component can not receive the route props.
    
    we can use hoc component to wrap child component
    so the component also can receive the route props

--- src/components/MyBlog/Post/Post.js
+++ src/components/MyBlog/Post/Post.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 
 import classes from './Post.css';
 
-const post = (props) => (
+const post = (props) => {
+  console.log(props)
+  return (
     <article className={classes.Post} onClick={props.clicked}>
         <h1>{props.title}</h1>
         <div className="Info">
             <div className={classes.Author}>{props.author}</div>
         </div>
     </article>
-);
+  )
+};
 
-export default post;
+export default withRouter(post);
