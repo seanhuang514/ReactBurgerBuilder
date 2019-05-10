ede6751e16f3b7be47aeb0b4fcb7bb51c718c849
sean huang <sean.huang@tarokosoftware.com>
2019-05-10 23:04:12 +0800

    238. Conditional Redirects (Redirect Component)

--- src/containers/MyBlog/NewPost/NewPost.js
+++ src/containers/MyBlog/NewPost/NewPost.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../../axios';
 import classes from './NewPost.css';
+import { Redirect } from 'react-router-dom';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
 
     postData = () => {
@@ -18,12 +20,22 @@ class NewPost extends Component {
       axios.post('/posts', payload)
         .then(response => {
           console.log(response);
+          this.setState({ submitted: true });
         })
+    }
+
+    get redirect() {
+      if(this.state.submitted) {
+        return <Redirect to="/posts" />
+      }else {
+        return null
+      }
     }
 
     render () {
         return (
             <div className={classes.NewPost}>
+                {this.redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
