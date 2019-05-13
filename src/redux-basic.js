b3464b6ecad8dc6555c831bd8ef9ce0f529f21d7
sean huang <sean.huang@tarokosoftware.com>
2019-05-14 00:23:03 +0800

    [note]284. Adding Subscriptions

--- src/redux-basic.js
+++ src/redux-basic.js
@@ -6,6 +6,11 @@ const initialState = {
 }
 
 //Reducer
+/* 
+  Reducer 負責根據 action.type 去決定要更改 state 裡的哪個 attribute
+  在 return state 的時候記得要是 immutable 的。就是要複製原本的 state 然後
+  修改之後再回傳。
+*/
 const rootReducer = (state = initialState, action) => {
   if(action.type === 'INC_COUNTER') {
     return {
@@ -25,13 +30,35 @@ const rootReducer = (state = initialState, action) => {
 }
 
 // Store
-
+/* 
+  用 Redux.createStore 來建立 store
+  initialize 的時候要傳入一個 reducer
+*/
 const store = createStore(rootReducer);
-console.log(store.getState())
+console.log('init store',store.getState())
+
+//Subscription
+/* 
+  監聽 state 的改變然後坐相對應的行為
+*/
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState());
+})
+
 
 //Dispatching Action
-
+/* 
+  發送消息 (message) 給 store 告訴他說要做什麼行為，然後 store 會交給 reducer 去實作
+*/
 store.dispatch({ type: 'INC_COUNTER'});
 store.dispatch({ type: 'ADD_COUNTER', value: 10});
-console.log(store.getState())
-//Subscription
+console.log('After Dispatch',store.getState())
+
+
+/* 
+init store { counter: 0 }
+[Subscription] { counter: 1 }
+[Subscription] { counter: 11 }
+After Dispatch { counter: 11 }
+
+*/
