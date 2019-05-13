76519159a3058645355e2d0062b2fb2788a933ba
sean huang <sean.huang@tarokosoftware.com>
2019-05-14 00:09:03 +0800

    [note]283. Dispatching Actions
    dispatch 的 object 裡一定要帶一個 type 的參數，值通常都是全大小的 string , 之後可以帶入想要帶的值。
    
    在 reducer 裡 return state 的時候記得一定要是 immutable ( copy original state the modify)

--- src/redux-basic.js
+++ src/redux-basic.js
@@ -1,3 +1,4 @@
+/* node require syntax */
 const Redux = require('redux')
 const createStore = Redux.createStore;
 const initialState = {
@@ -6,6 +7,20 @@ const initialState = {
 
 //Reducer
 const rootReducer = (state = initialState, action) => {
+  if(action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: ++state.counter
+    }
+  }
+
+  if(action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
+
   return state
 }
 
@@ -16,4 +31,7 @@ console.log(store.getState())
 
 //Dispatching Action
 
+store.dispatch({ type: 'INC_COUNTER'});
+store.dispatch({ type: 'ADD_COUNTER', value: 10});
+console.log(store.getState())
 //Subscription
