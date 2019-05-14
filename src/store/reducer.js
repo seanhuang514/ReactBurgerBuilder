bc5c428c0bde0b1b83f722ec4d540fcebc83afe3
sean huang <sean.huang@tarokosoftware.com>
2019-05-14 11:49:45 +0800

    289. Switch-Case in the Reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,34 +3,31 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-  if(action.type === 'INCREMENT') {
-    return {
-      ...state,
-      counter: state.counter + 1
-    }
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+    case 'DECREMENT':
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+    case 'ADD':
+      return {
+        ...state,
+        counter: state.counter + action.value
+      }
+    case 'SUBTRACT':
+      return {
+        ...state,
+        counter: state.counter - action.value
+      }
+    default:
+      return state
   }
 
-  if(action.type === 'DECREMENT') {
-    return {
-      ...state,
-      counter: state.counter - 1
-    }
-  }
-
-  if(action.type === 'ADD') {
-    return {
-      ...state,
-      counter: state.counter + action.value
-    }
-  }
-  
-  if(action.type === 'SUBTRACT') {
-    return {
-      ...state,
-      counter: state.counter - action.value
-    }
-  }
-  return state
 }
 
 export default reducer;
