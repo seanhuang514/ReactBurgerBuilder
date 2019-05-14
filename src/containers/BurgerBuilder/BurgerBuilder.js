ae568ad47940eaf491656cad385e1367e701c1ff
sean huang <sean.huang@tarokosoftware.com>
2019-05-14 16:10:17 +0800

    [note]302. Finishing the Reducer for Ingredients
    ```
    let keyName = 'mykey'
    obj = { [keyName]: 1 } #=> { myKey: 1 }
    ```

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,14 +19,14 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
-    axios
+    /* axios
       .get('/ingredients.json')
       .then(response => {
         this.setState({ ingredients: response.data });
       })
       .catch(error => {
         this.setState({ error: true });
-      });
+      }); */
   }
 
   updatePurchaseState(ingredients) {
--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,12 +1,42 @@
 import * as actionTypes from '../actions'
 
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    cheese: 0,
+    bacon: 0,
+    meat: 0
+  },
   totalPrice: 4,
 }
 
 const reducer = (state = initialState, action) => {
-  return state
+
+  switch(action.type){
+    case actionTypes.ADD_INGREDIENTS:
+      let ingredientName = action.ingredientName;
+
+      return {
+        ...state,
+        /* copy ingredients from state then override one of attribute */
+        ingredients: {
+          ...state.ingredients,
+          [ingredientName]: state.ingredients[ingredientName] + 1
+        }
+      }
+    case actionTypes.REMOVE_INGREDIENTS:
+      ingredientName = action.ingredientName;
+
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [ingredientName]: state.ingredients[ingredientName] - 1
+        }
+      }
+    default:
+    return state
+  }
 }
 
 export default reducer;
