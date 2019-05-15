1d4b810888f1d31fbb769ea3d9a96e7c124628c5
sean huang <sean.huang@tarokosoftware.com>
2019-05-15 17:24:42 +0800

    320. A Leaner Switch Case Statement

--- src/store/redux101/reducers/result.js
+++ src/store/redux101/reducers/result.js
@@ -5,24 +5,24 @@ const initialState = {
   results:[]
 }
 
+const deleteResult = (state, action) => {
+  const new_arr = state.results.filter(result => result.id !== action.resultId)
+  return updateObject(state, { results: new_arr })
+}
+
+const storeResult = (state, action) => {
+  // immutable way to push element to array and return new array
+  const results = state.results.concat({ id: new Date(), value: action.result })
+  return updateObject(state, { results: results })
+}
+
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case actionType.INCREMENT: 
       console.log('result!!!')
       return state
-    case actionType.STORE_RESULT:
-      // immutable way to push element to array and return new array
-      return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
-    case actionType.DELETE_RESULT:
-      /* way1
-      const id = 2;
-      const new_arr = [...state.results]
-      new_arr.splice(id, 1) 
-      */
-
-      // way 2
-      const new_arr = state.results.filter(result => result.id !== action.resultId)
-      return updateObject(state, { results: new_arr })
+    case actionType.STORE_RESULT : return storeResult(state, action);
+    case actionType.DELETE_RESULT : return deleteResult(state, action);
     default:
       return state
   }
