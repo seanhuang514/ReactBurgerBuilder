e87db9f072976a5256ab305afe057d7cf76462cd
sean huang <sean.huang@tarokosoftware.com>
2019-05-17 00:32:49 +0800

    350. Adding an Auth Form(part2)
    Add navigation button and routes

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const NavigationItems = props => (
   <ul className={classes.navigationItems}>
     <NavigationItem link='/'>Burger Builder</NavigationItem>
     <NavigationItem link='/orders'>Orders</NavigationItem>
+    <NavigationItem link='/auth'>Authenticate</NavigationItem>
   </ul>
 );
 
