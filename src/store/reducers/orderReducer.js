7a6e9b927e2432d965eb65f1a79355e61d351ee6
sean huang <sean.huang@tarokosoftware.com>
2019-05-16 12:40:56 +0800

    335. The Order Reducer

---
+++ src/store/reducers/orderReducer.js
@@ -0,0 +1,31 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initState = {
+  orders: [],
+  loading: false
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case actionTypes.PURCHASE_BURGER_SUCCESS:
+      const new_order = {
+        ...action.orderData,
+        id: action.orderId
+      }
+
+      return {
+        ...state,
+        orders: state.orders.concat(new_order),
+        loading: false
+      }
+    case actionTypes.PURCHASE_BURGER_FAILED: 
+      return {
+        ...state,
+        loading: false
+      }
+    default:
+      return state;
+  }
+}
+
+export default reducer;
