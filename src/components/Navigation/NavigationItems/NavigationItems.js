4aaddba4db4be8735f97c3fde4cb4ec9cb1798e2
sean huang <sean.huang@tarokosoftware.com>
2019-05-17 14:33:10 +0800

    358. Updating the UI Depending on Auth State

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem'
+import { connect } from 'react-redux'
 
 const NavigationItems = props => (
   <ul className={classes.navigationItems}>
     <NavigationItem link='/'>Burger Builder</NavigationItem>
     <NavigationItem link='/orders'>Orders</NavigationItem>
-    <NavigationItem link='/auth'>Authenticate</NavigationItem>
+    { props.isAuthenticate ? <NavigationItem link='/auth'>Logout</NavigationItem> : <NavigationItem link='/auth'>Authenticate</NavigationItem>}
+    
   </ul>
 );
 
-export default NavigationItems;
+const mapStateToProps = state => {
+  return {
+    isAuthenticate: state.auth.token !== null
+  }
+}
+
+export default connect(mapStateToProps)(NavigationItems);
