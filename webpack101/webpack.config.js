9cdadd69cbd564bc4f8dd3920d0d8b791883aeb1
sean huang <sean.huang@tarokosoftware.com>
2019-05-22 14:20:31 +0800

    407. Adding File Rules

--- webpack101/webpack.config.js
+++ webpack101/webpack.config.js
@@ -7,5 +7,18 @@ module.exports = {
     path: path.resolve('__dirname', 'dist'),
     filename: 'bundle.js',
     publicPath: ''
+  },
+  resolve: {
+    /* 當 import file 沒有指定副檔名時該抓哪些種類的副檔名 import 進來 */
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader:
+        exclude: /node_modules/
+      }
+    ]
   }
 }
