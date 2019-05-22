e844c5a5d76df15b13e4edcb90d0facaffba184a
sean huang <sean.huang@tarokosoftware.com>
2019-05-22 14:48:34 +0800

    409. Adding CSS File Support

--- webpack101/webpack.config.js
+++ webpack101/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'cheap-module-source-map',
@@ -18,6 +19,43 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          /* 
+            * IMPORTANT
+            webpack parses loaders in this use array and applies them from right to left,
+            so from bottom to top if we write it like this.
+          */
+          /* yarn add css-loader style-loader --dev */
+          { loader: 'style-loader' },
+          { 
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'/* [] */
+            }
+          },
+          { 
+            loader: 'postcss-loader', /* yarn add postcss-loader --dev */
+            options: {
+              ident: 'postcss',
+              plugins: () => {
+                /* yarn add autoprefixer --dev  */
+                autoprefixer({
+                  browsers: [ 
+                    /* https://github.com/browserslist/browserslist */
+                    "> 1%",
+                    "last 2 versions"
+                  ]
+                })
+              }
+            }
+          }
+        ]
       }
     ]
   }
