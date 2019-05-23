7d8be759bf72b3f93a08a7260db7b0051dd3f21c
sean huang <sean.huang@tarokosoftware.com>
2019-05-23 11:49:04 +0800

    434. Wrapping the Transition Component

--- animations101/src/App.js
+++ animations101/src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
-import Transition from 'react-transition-group/Transition';
 
 class App extends Component {
   state = {
@@ -23,16 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Transition 
-          mountOnEnter
-          unmountOnExit
-          in={this.state.modalIsOpen}
-          timeout={300}>
-          {state => (
-            <Modal show={state} closed={this.closeModal} />
-          )}
-        </Transition>
-        
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
--- animations101/src/components/Modal/Modal.js
+++ animations101/src/components/Modal/Modal.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
 const modal = (props) => {
-  const classes = [
-    'Modal',
-    props.show === 'entering' 
-      ? 'ModalOpen' 
-      : props.show === 'exiting' ? 'ModalClose' : null 
-  ]
+  const dynamicClasses = (state) => {
+    let classes = ['Modal'];
+
+    if(state === 'entering') {
+      classes.push('ModalOpen')
+    }else if(state === 'exiting'){
+      classes.push('ModalClose')
+    }
+
+    return classes.join(' ')
+  }
 
   return (
-    <div className={classes.join(' ')}>
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
+    <Transition  mountOnEnter unmountOnExit in={props.show} timeout={400}>
+      {state => (
+        <div className={dynamicClasses(state)}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>Dismiss</button>
+        </div>
+      )}
+    </Transition>
   )
 };
 
