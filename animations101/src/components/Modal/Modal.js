e0fc5600ddfc8882ba6d357802b00625b979ae91
sean huang <sean.huang@tarokosoftware.com>
2019-05-23 12:00:04 +0800

    436. Transition Events

--- animations101/src/components/Modal/Modal.js
+++ animations101/src/components/Modal/Modal.js
@@ -22,7 +22,18 @@ const modal = (props) => {
   }
 
   return (
-    <Transition  mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+      onEnter={() => console.log('onEnter')}
+      onEntering={() => console.log('onEntering')}
+      onEntered={() => console.log('onEntered')}
+      onExit={() => console.log('onExit')}
+      onExiting={() => console.log('onExiting')}
+      onExited={() => console.log('onExited')}
+      >
       {state => (
         <div className={dynamicClasses(state)}>
             <h1>A Modal</h1>
