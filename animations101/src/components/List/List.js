6ce423b7a71811848d7d844d628ce22b85512971
sean huang <sean.huang@tarokosoftware.com>
2019-05-23 12:33:04 +0800

    439. Animating Lists

--- animations101/src/components/List/List.js
+++ animations101/src/components/List/List.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TransitionGroup from 'react-transition-group/TransitionGroup'
+import CSSTransition from 'react-transition-group/CSSTransition'
 
 import './List.css';
 
@@ -25,19 +27,20 @@ class List extends Component {
 
     render () {
         const listItems = this.state.items.map( (item, index) => (
+          <CSSTransition classNames="item" key={index} timeout={500}>
             <li 
-                key={index}
-                className="ListItem" 
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
+              className="ListItem" 
+              onClick={() => this.removeItemHandler(index)}>{item}</li>
+          </CSSTransition>
         ) );
 
         return (
             <div>
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
-                    {listItems}
-                </ul>
+                <TransitionGroup component="ul" className="List">
+                  {listItems}
+                </TransitionGroup>
             </div>
         );
     }
